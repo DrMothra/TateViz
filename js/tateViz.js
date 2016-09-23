8fd94665c805b47d59f70b3124199e0c09c6ac36
Tony Glover <tony.glover@nottingham.ac.uk>
2016-09-23 15:06:24 +0100

    Initial framework commit

---
+++ js/tateViz.js
@@ -0,0 +1,62 @@
+/**
+ * Created by DrTone on 04/12/2014.
+ */
+//Visualisation framework
+
+//Init this app from base
+function Tate() {
+    BaseApp.call(this);
+}
+
+Tate.prototype = new BaseApp();
+
+Tate.prototype.init = function(container) {
+    BaseApp.prototype.init.call(this, container);
+    //GUI
+    this.guiControls = null;
+    this.gui = null;
+};
+
+Tate.prototype.createScene = function() {
+    //Create scene
+    BaseApp.prototype.createScene.call(this);
+
+    //Load example object
+    var boxGeom = new THREE.BoxGeometry(10, 10, 10);
+    var mat = new THREE.MeshPhongMaterial({color: 0xb5b5b5, transparent: false, opacity: 0.5});
+    var box = new THREE.Mesh(boxGeom, mat);
+    this.scene.add(box);
+};
+
+Tate.prototype.createGUI = function() {
+    //GUI - using dat.GUI
+    this.guiControls = new function() {
+
+    };
+
+    var gui = new dat.GUI();
+
+    //Add some folders
+    this.guiAppear = gui.addFolder("Appearance");
+    this.guiData = gui.addFolder("Data");
+    this.gui = gui;
+};
+
+Tate.prototype.update = function() {
+    //Perform any updates
+
+    BaseApp.prototype.update.call(this);
+};
+
+$(document).ready(function() {
+    //Initialise app
+    var container = document.getElementById("WebGL-output");
+    var app = new Tate();
+    app.init(container);
+    app.createScene();
+    app.createGUI();
+
+    //GUI callbacks
+
+    app.run();
+});
