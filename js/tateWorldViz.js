c3889d7ce16fca1c041244c885130e8b42517b1a
Tony Glover <tony.glover@nottingham.ac.uk>
2016-10-21 14:49:18 +0100

    Draw nodes on map, adjust lighting

--- js/tateWorldViz.js
+++ js/tateWorldViz.js
@@ -2,6 +2,8 @@
  * Created by tonyg on 20/10/2016.
  */
 
+var X_AXIS=0, Y_AXIS=1, Z_AXIS=2;
+
 //Init this app from base
 function Tate() {
     BaseApp.call(this);
@@ -23,17 +25,86 @@ Tate.prototype.createScene = function() {
     BaseApp.prototype.createScene.call(this);
 
     //Camera
-    this.camera.position.set(0, 20, 20);
+    var camPos = 1000;
+    this.camera.position.set(0, camPos, camPos);
 
     //Model loading
+    this.xMax = 1000;
+    this.xMin = -1000;
+    this.yMax = 500;
+    this.yMin = -500;
+
     this.loader = new THREE.JSONLoader();
 
     this.loader.load('./models/worldBase.json', function (geometry, materials) {
         var mesh = new THREE.Mesh(geometry, new THREE.MultiMaterial(materials));
-        //mesh.rotation.x = -Math.PI/2;
+        mesh.scale.set(50, 1, 50);
+        var bbox = new THREE.Box3().setFromObject(mesh);
+
         _this.scenes[_this.currentScene].add(mesh);
     });
 
+    //Get data
+    var i, nodeRadius = 5, nodeSegments = 24;
+    var sphereGeom = new THREE.SphereBufferGeometry(nodeRadius, nodeSegments, nodeSegments);
+    var sphereMat = new THREE.MeshLambertMaterial( {color: 0xffff00} );
+    var mainGroup = new THREE.Object3D();
+    var pos = new THREE.Vector3(), ground = new THREE.Vector3();
+    var mesh;
+    //var numNodes = tateData.length;
+    var numNodes = 2;
+    for(i=0; i<numNodes; ++i) {
+        pos = this.getNodePosition(tateData[i]["Location coordinates"], tateData[i]["Start"]);
+        if(pos !== undefined) {
+            mesh = new THREE.Mesh(sphereGeom, sphereMat);
+            mesh.position.copy(pos);
+            ground.copy(pos);
+            ground.y = 0;
+            this.drawLink(pos, ground, mainGroup, LINES.LineColours['yellow']);
+            mainGroup.add(mesh);
+        }
+    }
+    this.scenes[this.currentScene].add(mainGroup);
+};
+
+Tate.prototype.getNodePosition = function(location, date) {
+    //Determine position from lat long
+    if(!location) return undefined;
+
+    var lat = parseFloat(location);
+    if(isNaN(lat)) {
+        console.log("Invalid latitude!");
+        return undefined;
+    }
+    var longIndex = location.indexOf(",");
+    if(longIndex < 0) {
+        console.log("Invalid longitude");
+        return undefined;
+    }
+    var long = location.substr(longIndex+1, location.length-longIndex);
+    long = parseFloat(long);
+    if(isNaN(long)) {
+        console.log("Invalid longitude");
+        return undefined;
+    }
+
+    lat = (lat/90)*this.yMax;
+    long = (long/180)*this.xMax;
+    date = date.slice(-4);
+    date = parseFloat(date);
+    if(isNaN(date)) {
+        console.log("Invalid date!");
+        return undefined;
+    }
+    date -= 1960;
+    return new THREE.Vector3(long, date, -lat);
+};
+
+Tate.prototype.drawLink = function(from, to, group, lineColour) {
+    var lineGeom = new THREE.Geometry();
+    lineGeom.vertices.push(from, to);
+    var link = new THREE.Line(lineGeom, lineColour);
+    group.add(link);
 };
 
 Tate.prototype.createGUI = function() {
@@ -43,6 +114,9 @@ Tate.prototype.createGUI = function() {
         this.Background = '#5c5f64';
         this.LabelWidth = 1.0;
         this.LabelHeight = 1.0;
+        this.LightX = 0;
+        this.LightY = 50;
+        this.LightZ = -600;
     };
 
     var gui = new dat.GUI();
@@ -51,6 +125,22 @@ Tate.prototype.createGUI = function() {
     this.guiAppear = gui.addFolder("Appearance");
     this.guiAppear.addColor(this.guiControls, 'Background').onChange(function (value) {
         _this.renderer.setClearColor(value, 1.0);
+    });
+
+    //Light pos
+    var lightX = gui.add(this.guiControls, 'LightX', -1000, 1000).step(10);
+    lightX.onChange(function(value) {
+        _this.onLightChange(X_AXIS, value);
+    });
+
+    var lightY = gui.add(this.guiControls, 'LightY', 0, 400).step(10);
+    lightY.onChange(function(value) {
+        _this.onLightChange(Y_AXIS, value);
+    });
+
+    var lightZ = gui.add(this.guiControls, 'LightZ', -1000, 200).step(10);
+    lightZ.onChange(function(value) {
+        _this.onLightChange(Z_AXIS, value);
     });
 
     this.gui = gui;
@@ -62,6 +152,31 @@ Tate.prototype.update = function() {
     var delta = this.clock.getDelta();
 
     BaseApp.prototype.update.call(this);
+};
+
+Tate.prototype.onLightChange = function(axis, pos) {
+    //Alter light pos
+    var light = this.scenes[this.currentScene].getObjectByName("PointLight");
+    if(!light) {
+        console.log("Couldn't get light!");
+        return;
+    }
+    switch (axis) {
+        case X_AXIS:
+            light.position.x = pos;
+            break;
+
+        case Y_AXIS:
+            light.position.y = pos;
+            break;
+
+        case Z_AXIS:
+            light.position.z = pos;
+            break;
+
+        default:
+            break;
+    }
 };
 
 $(document).ready(function() {
