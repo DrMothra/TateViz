0048cc2174ab49dbbd9e54e0babca375100c0c4c
Tony Glover <tony.glover@nottingham.ac.uk>
2016-11-18 11:55:57 +0000

    Completed first iteration of build process

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
+var cache = require('gulp-cache');
 
 gulp.task('jshint', function() {
     return gulp.src(['./js/*.js'])
@@ -16,10 +18,41 @@ gulp.task('jshint', function() {
         .pipe(jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('build', function() {
+gulp.task('build', ['minify', 'sync'], function() {
+
+});
+
+gulp.task('minify', function() {
     return gulp.src('tateWorldViz.html')
         .pipe(useref())
         .pipe(gulpIf('*.js', uglify()))
         .pipe(gulpIf('*.css', cssnano()))
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('sync', function() {
+    return gulp.src('models/*.json')
+        .pipe(gulp.dest('dist/models'))
+});
+
+gulp.task('buildimages', function() {
+    return gulp.src('images/*.+(png|jpg)')
+        .pipe(cache(imagemin()))
+        .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('buildmodels', function() {
+    return gulp.src('models/*.+(png|jpg)')
+        .pipe(cache(imagemin()))
+        .pipe(gulp.dest('dist/models'))
+});
+
+gulp.task('buildtextures', function() {
+    return gulp.src('textures/*.+(png|jpg)')
+        .pipe(cache(imagemin()))
+        .pipe(gulp.dest('dist/textures'))
+});
+
+gulp.task('buildAllImages', ['buildimages', 'buildmodels', 'buildtextures'], function() {
+
 });
